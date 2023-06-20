bba338249ff402e292257c4e30bf983832a2a282
Neethu Kunjappan <neethu.kunjappan@tu_clausthal.de>
2023-06-20 20:46:05 +0530

    done

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-const express = require("express")
-const app = express()
-const port = 80
+const express = require("express");
+const app = express();
+const port = 80;
 
-
-app.use(express.static(__dirname + "/public"))
-
-
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-	console.log("This is the homepage")
+  console.log("This is the homepage");
 
-
-	console.log(__dirname)
-	res.sendFile(__dirname + "/views/home.html")
-})
+  // console.log(__dirname);
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 app.get("/about", (req, res) => {
-	res.sendFile(__dirname + "/views/about.html")
-})
+  res.sendFile(__dirname + "/views/about.html");
+});
 
 app.get("/works", (req, res) => {
-	res.sendFile(__dirname + "/views/works.html")
-})
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
 
 app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
-	console.log(`Listening on port ${port}`)
-})
+// Define routes
+//app.get('/', (req, res) => {
+//res.send('Hello, world!');
+//});
+
+//app.get('/about', (req, res) => {
+//res.send('About page');
+//});
+
+// Start the server
+//app.listen(port, () => {
+//console.log(`Server is running on port ${port}`);
+//});
